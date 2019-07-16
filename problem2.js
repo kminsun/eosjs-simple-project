39f03889e8c90929f7b3426bb14c0fc372d53946
kminsun <spdlqj201802@naver.com>
2019-07-16 16:14:31 +0900

    init

---
+++ problem2.js
@@ -0,0 +1,20 @@
+const { Api, JsonRpc, RpcError } = require('eosjs');
+const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig');
+const { PrivateKey, PublicKey, Signature, Aes, key_utils, config } = require('eosjs-ecc');
+const { TextEncoder, TextDecoder } = require('util');
+const fetch = require('node-fetch');
+
+let privateKeys = [];// user private keys
+const rpc = new JsonRpc('http://192.168.1.75:8010', { fetch });
+const signatureProvider = new JsSignatureProvider(privateKeys);
+const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+
+async function main() {
+    try {
+        const result = await rpc.get_account('baekseok1115');
+        console.log(result.created);
+    } catch (error) {
+        console.error(error);
+    }
+}
+main();
